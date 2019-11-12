3dea94c4eb2c92766d529d917d0adaae7666f6ed
Jacob <jacob.siler91@gmail.com>
2019-11-12 15:35:23 +0000

    Update project settings, and terrains.

--- Assets/UI/scripts/WorldMap.cs
+++ Assets/UI/scripts/WorldMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b6cd9cfd37a3f6c67e685680f09acea39f70db0fb02113c132395ffe54b7b5f
-size 1521
+oid sha256:22af5a338682b611a5121c69020660d317b899e1a12df767888c55f822d1ea5f
+size 1434
